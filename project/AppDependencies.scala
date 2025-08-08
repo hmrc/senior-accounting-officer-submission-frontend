69874d23d7278f716f19485f2f669584894c35f5
rishisachania <19263328+rishisachania@users.noreply.github.com>
2025-08-08 14:42:53 +0100

    Lib bumps and sbt build changes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,20 +5,21 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.18.0"
+  private val hmrcMongoVersion = "2.7.0"
+  private val hmrcPlayFrontend = "12.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.6.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % hmrcPlayFrontend,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion % Test,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion % Test,
-    "org.jsoup"          % "jsoup"                   % "1.20.1"         % Test
-  )
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
+    "org.scalatestplus" %% "scalacheck-1-18"         % "3.2.19.0"
+  ).map(_ % Test)
 
-  val it: Seq[ModuleID] = Seq.empty
+  def apply(): Seq[ModuleID] = compile ++ test
 }
