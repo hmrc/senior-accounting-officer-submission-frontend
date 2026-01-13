475654cc5556c90d174febda420466b9da2a17e4
BradBarfoot <38399164+CyborgAeon@users.noreply.github.com>
2026-01-13 11:55:27 +0000

    add tests to prove the notificationcheckyouranswers service provides a summaryList regardless of data on the userAnswers

---
+++ test/services/NotificationCheckYourAnswersServiceSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package services
+
+import base.SpecBase
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
+import pages.NotificationAdditionalInformationPage
+import play.api.i18n.MessagesApi
+import play.api.test.FakeRequest
+import play.api.i18n.Messages
+import viewmodels.checkAnswers.NotificationAdditionalInformationSummary
+
+class NotificationCheckYourAnswersServiceSpec extends SpecBase with GuiceOneAppPerSuite {
+  "NotificationCheckYourAnswersService must generate the summaryList when all the userAnswers" - {
+    def SUT        = app.injector.instanceOf[NotificationCheckYourAnswersService]
+    given Messages = app.injector.instanceOf[MessagesApi].preferred(FakeRequest())
+
+    "are present" in {
+      val userAnswers = emptyUserAnswers.set(NotificationAdditionalInformationPage, Some("someValue")).get
+      SUT.getSummaryList(userAnswers) mustBe SummaryList(
+        Seq(
+          NotificationAdditionalInformationSummary.row(userAnswers)
+        )
+      )
+    }
+
+    "are empty" in {
+      SUT.getSummaryList(emptyUserAnswers) mustBe SummaryList(
+        Seq(
+          NotificationAdditionalInformationSummary.row(emptyUserAnswers)
+        )
+      )
+    }
+  }
+}
