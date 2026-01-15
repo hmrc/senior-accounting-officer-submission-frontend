4e1cb6f3867093022dfc3a3a77a812ecc8b823f0
Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>
2026-01-15 18:46:44 +0000

    updated datePage scaffold for to include viewmodels.checkAnswers Spec

---
+++ .g8/datePage/generated-test/viewmodels/checkAnswers/$className$SummarySpec.scala
@@ -0,0 +1,73 @@
+package viewmodels.checkAnswers
+
+import base.SpecBase
+import controllers.routes
+import models.CheckMode
+import org.scalatestplus.play.guice.GuiceOneAppPerSuite
+import pages.$className$Page
+import play.api.i18n.{Messages, MessagesApi}
+import uk.gov.hmrc.govukfrontend.views.Implicits.RichString
+
+import java.time.LocalDate
+
+class $className$SummarySpec extends SpecBase with GuiceOneAppPerSuite {
+  given Messages = app.injector.instanceOf[MessagesApi].preferred(Seq.empty)
+
+  "$className$Summary.row" - {
+
+    "when there is no answer for $className$Page" - {
+      "must return None" in {
+        def SUT = $className$Summary.row(emptyUserAnswers)
+
+        SUT mustBe None
+      }
+    }
+
+    "when there is a user answer for $className$Page" - {
+      def testUserAnswers(answer: LocalDate) =
+        emptyUserAnswers.set($className$Page, answer).get
+
+      def SUT(answer: LocalDate = LocalDate.now) = $className$Summary.row(testUserAnswers(answer)).get
+
+      "must have expected key" in {
+        SUT().key mustBe "$className$".toKey
+      }
+
+      "expected value" - {
+        "must show '1 January 2000' when user answers is 1st Jan 2000" in {
+          SUT(answer = LocalDate.of(2000, 1, 1)).value.content mustBe "1 January 2000".toText
+        }
+      }
+
+      "expected action" - {
+        def actions = SUT().actions
+
+        "must only have one action" in {
+          withClue("must be 1 action\n") {
+            actions.size mustBe 1
+          }
+          withClue("must be 1 item in the action\n") {
+            actions.head.items.size mustBe 1
+          }
+        }
+
+        def action = actions.head.items.head
+
+        "must have expected text" in {
+          action.content mustBe "Change".toText
+        }
+
+        "must have expected url" in {
+          action.href mustBe routes.$className$Controller
+            .onPageLoad(CheckMode)
+            .url
+        }
+
+        "must have expected hidden text" in {
+          action.visuallyHiddenText.get mustBe "$className$"
+        }
+      }
+    }
+  }
+
+}
