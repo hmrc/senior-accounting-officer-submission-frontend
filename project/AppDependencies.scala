e6e2c166d1e54495b3e1e0dfa9c40fec6b639406
Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>
2026-01-16 10:48:31 +0000

    added Excel & POI dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,11 +8,14 @@ object AppDependencies {
   private val bootstrapVersion = "9.19.0"
   private val hmrcMongoVersion = "2.7.0"
   private val hmrcPlayFrontend = "12.8.0"
+  private val poiVersion       = "5.5.1"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % hmrcPlayFrontend,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
+    "org.apache.poi"     % "poi"                        % poiVersion,
+    "org.apache.poi"     % "poi-ooxml"                  % poiVersion
   )
 
   val test: Seq[ModuleID] = Seq(
