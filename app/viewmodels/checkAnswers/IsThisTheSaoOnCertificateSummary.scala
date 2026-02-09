0d57ac4372acf6aa4d7e20cbdccadbbd94825b1d
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-02-09 13:55:57 +0000

    is this the sao summary row add data test id

--- app/viewmodels/checkAnswers/IsThisTheSaoOnCertificateSummary.scala
+++ app/viewmodels/checkAnswers/IsThisTheSaoOnCertificateSummary.scala
@@ -20,9 +20,10 @@ import controllers.routes
 import models.{CheckMode, UserAnswers}
 import pages.IsThisTheSaoOnCertificatePage
 import play.api.i18n.Messages
-import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.converters.*
 import viewmodels.govuk.summarylist.*
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Key, SummaryListRow}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 
 object IsThisTheSaoOnCertificateSummary {
 
@@ -32,8 +33,14 @@ object IsThisTheSaoOnCertificateSummary {
       val value = if answer then "site.yes" else "site.no"
 
       SummaryListRowViewModel(
-        key = messages("isThisTheSaoOnCertificate.checkYourAnswersLabel").toKey,
-        value = ValueViewModel(messages(value).toText),
+        key = Key(
+          HtmlContent(
+            s"""<span data-test-id="is-this-the-sao-key">${messages(
+                "isThisTheSaoOnCertificate.checkYourAnswersLabel"
+              )}</span>"""
+          )
+        ),
+        value = ValueViewModel(HtmlContent(s"""<span data-test-id="is-this-the-sao-value">${messages(value)}</span>""")),
         actions = Seq(
           ActionItemViewModel(
             messages("site.change").toText,
