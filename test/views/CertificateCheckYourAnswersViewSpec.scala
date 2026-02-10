c29e49561a672c171ad1a598b672ef2d95b9efd6
dananjaiahnemmadi <202163272+dananjaiahnemmadi@users.noreply.github.com>
2026-02-10 12:14:41 +0000

    Saod 561 - update Check Your Answers page in view and messages (#39)
    * wip update Check Your Answers page in view and messages
    
    * wip added summary list
    
    * update to include the table of data
    
    * add empty CertificateCheckYourAnswersServiceSpec
    
    * skeleton for service spec
    
    * complete certificate view spec
    
    * wip cert service spec
    
    * wip - certificateCheckYourAnswersSpec
    
    * wip - updated journey when changing 'isThisTheSAO'
    
    * added data test id's for change links
    
    * add id to Full Name key and lint
    
    * Fixing test
    
    * WIP - Working on checkYourAnswersTests
    
    * WIP - Working on IsThisTheSAO page
    
    * WIP - Unit Tests completed
    
    * WIP - revert to messages key
    
    * WIP add missing test
    
    * change test keys
    
    * change id name
    
    * WIP - fixed failing test
    
    * add id to value
    
    * is this the sao summary row add data test id
    
    * updated view models
    
    * Started helper func
    
    * Revert "Started helper func"
    
    This reverts commit 500cecfc4891a2c8b784d2b8b43472baff70d9a7.
    
    * remove unused repository
    
    ---------
    
    Co-authored-by: AprilGar <april.garlington@hotmail.co.uk>
    Co-authored-by: rishisachania <19263328+rishisachania@users.noreply.github.com>
    Co-authored-by: Nikhilvedi <7196727+Nikhilvedi@users.noreply.github.com>
    Co-authored-by: jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>

--- test/views/CertificateCheckYourAnswersViewSpec.scala
+++ test/views/CertificateCheckYourAnswersViewSpec.scala
@@ -19,16 +19,22 @@ package views
 import base.ViewSpecBase
 import controllers.routes
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
+import org.jsoup.nodes.{Document, Element}
+import org.jsoup.select.Elements
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.{Key, Value as SLValue, *}
+import viewmodels.converters.*
+import viewmodels.govuk.all.{ActionItemViewModel, SummaryListRowViewModel, withVisuallyHiddenText}
 import views.CertificateCheckYourAnswersViewSpec.*
+import views.NotificationCheckYourAnswersViewSpec.testValue1
 import views.html.CertificateCheckYourAnswersView
 
 class CertificateCheckYourAnswersViewSpec extends ViewSpecBase[CertificateCheckYourAnswersView] {
 
-  private def generateView(): Document = Jsoup.parse(SUT().toString)
+  private def generateView(summaryList: SummaryList): Document = Jsoup.parse(SUT(summaryList).toString)
 
   "CertificateCheckYourAnswersView" - {
-    val doc: Document = generateView()
+    val summaryList   = SummaryList()
+    val doc: Document = generateView(summaryList)
 
     doc.createTestsWithStandardPageElements(
       pageTitle = pageTitle,
@@ -38,16 +44,156 @@ class CertificateCheckYourAnswersViewSpec extends ViewSpecBase[CertificateCheckY
       hasError = false
     )
 
+    doc.createTestsWithCaption(
+      pageCaption
+    )
+
     doc.createTestsWithOrWithoutError(hasError = false)
 
     doc.createTestsWithSubmissionButton(
       action = routes.CertificateCheckYourAnswersController.onSubmit(),
-      buttonText = "Continue"
+      buttonText = pageButtonText
     )
+  }
+
+  "non-empty summary list must result in a table with rows" - {
+    val summaryList =
+      SummaryList(rows =
+        Seq(
+          SummaryListRowViewModel(
+            key = testKey1.toKey,
+            value = SLValue(testValue1.toText),
+            actions = Seq(
+              ActionItemViewModel(
+                testActionMessage1.toText,
+                testActionUrl1
+              )
+                .withVisuallyHiddenText(testActionHiddenText1)
+            )
+          ),
+          SummaryListRowViewModel(
+            key = testKey2.toKey,
+            value = SLValue(testValue2.toText)
+          )
+        )
+      )
+
+    val doc: Document = generateView(summaryList)
+
+    "Description List" - {
+      "must be present" in {
+        doc.descriptionLists.size() mustBe 1
+      }
+
+      "must have two rows" in {
+        doc.descriptionList.getElementsByClass("govuk-summary-list__row").size() mustBe 2
+      }
+
+      "must generate correct content when there is a call to action" in {
+        validateSummaryListRow(
+          row = doc.descriptionList.getElementsByClass("govuk-summary-list__row").get(0),
+          keyText = testKey1,
+          valueText = testValue1,
+          actionText = testActionMessage1,
+          actionHiddenText = testActionHiddenText1,
+          actionHref = testActionUrl1
+        )
+      }
+
+      "must generate correct content when there is no call to action" in {
+        validateSummaryListRowNoAction(
+          row = doc.descriptionList.getElementsByClass("govuk-summary-list__row").get(1),
+          keyText = testKey2,
+          valueText = testValue2
+        )
+      }
+
+      "description list must have css class 'govuk-summary-list'" in {
+        doc.descriptionList.hasClass("govuk-summary-list") mustBe true
+      }
+    }
+  }
+
+  def validateSummaryListRow(
+      row: Element,
+      keyText: String,
+      valueText: String,
+      actionText: String,
+      actionHiddenText: String,
+      actionHref: String
+  ): Unit = {
+    val key = row.select("dt.govuk-summary-list__key")
+    key.size() mustBe 1
+    withClue("row keyText mismatch:\n") {
+      key.get(0).text() mustBe keyText
+    }
+
+    val value = row.select("dd.govuk-summary-list__value")
+    value.size() mustBe 1
+    withClue("row valueText mismatch:\n") {
+      value.get(0).text() mustBe valueText
+    }
+
+    val action = row.select("dd.govuk-summary-list__actions")
+    withClue("row action not found!:\n") {
+      action.size() mustBe 1
+    }
+
+    val linkText = action.get(0).select("a")
+    linkText.size() mustBe 1
+    withClue("row actionHref mismatch:\n") {
+      linkText.get(0).attr("href") mustBe actionHref
+    }
+    withClue("row actionHiddenText mismatch:\n") {
+      linkText.get(0).select("span.govuk-visually-hidden").text() mustBe actionHiddenText
+    }
+    linkText.get(0).select("span.govuk-visually-hidden").remove()
+    withClue("row actionText mismatch:\n") {
+      linkText.get(0).text() mustBe actionText
+    }
+  }
+
+  def validateSummaryListRowNoAction(
+      row: Element,
+      keyText: String,
+      valueText: String
+  ): Unit = {
+    val key = row.select("dt.govuk-summary-list__key")
+    key.size() mustBe 1
+    withClue("row keyText mismatch:\n") {
+      key.get(0).text() mustBe keyText
+    }
+
+    val value = row.select("dd.govuk-summary-list__value")
+    value.size() mustBe 1
+    withClue("row valueText mismatch:\n") {
+      value.get(0).text() mustBe valueText
+    }
+
+    val action = row.select("dd.govuk-summary-list__actions")
+    withClue("row action found!:\n") {
+      action.size() mustBe 0
+    }
+  }
+
+  extension (target: Document | Element) {
+    def summaryListGrid: Elements    = target.select("div.govuk-grid-row")
+    def summaryListContent: Elements = summaryListGrid.select("div.govuk-grid-column-two-thirds")
+    def descriptionLists: Elements   = summaryListContent.select("dl")
+    def descriptionList: Element     = descriptionLists.get(0)
   }
 }
 
 object CertificateCheckYourAnswersViewSpec {
-  val pageHeading = "certificateCheckYourAnswers"
-  val pageTitle   = "certificateCheckYourAnswers"
+  val pageHeading           = "Check your answers"
+  val pageTitle             = "Submit a certificate"
+  val pageCaption           = "Submit a certificate"
+  val pageButtonText        = "Continue"
+  val testKey1              = "testKey"
+  val testValue1            = "nonEmptyString"
+  val testActionMessage1    = "dummyMessage"
+  val testActionUrl1        = "dummyUrl"
+  val testActionHiddenText1 = "dummyVisuallyHiddenText"
+  val testKey2              = "testKey2"
+  val testValue2            = "nonEmptyString2"
 }
