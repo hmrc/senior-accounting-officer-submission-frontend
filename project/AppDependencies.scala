0191da9071aac34739fdc6d32076cbd08e39ecf2
Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>
2026-04-23 14:15:10 +0100

    poc upload pdf to object store & download from it

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,13 +12,14 @@ object AppDependencies {
   private val scalaCsvVersion  = "2.0.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"            %% "play-frontend-hmrc-play-30" % hmrcPlayFrontend,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-play-30"         % hmrcMongoVersion,
-    "org.apache.poi"          % "poi"                        % poiVersion,
-    "org.apache.poi"          % "poi-ooxml"                  % poiVersion,
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"  % bootstrapVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"  % hmrcPlayFrontend,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"          % hmrcMongoVersion,
+    "org.apache.poi"           % "poi"                         % poiVersion,
+    "org.apache.poi"           % "poi-ooxml"                   % poiVersion,
     "com.github.tototoshi"   %% "scala-csv"                  % scalaCsvVersion,
-    "io.github.openhtmltopdf" % "openhtmltopdf-pdfbox"       % "1.1.37"
+    "io.github.openhtmltopdf"  % "openhtmltopdf-pdfbox"        % "1.1.37",
+    "uk.gov.hmrc.objectstore" %% "object-store-client-play-30" % "2.5.0"
   )
 
   val test: Seq[ModuleID] = Seq(
