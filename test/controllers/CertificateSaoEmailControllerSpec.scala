257022ea7b253c4549751f13d00fafdc65c95ee2
calebnartey <72252764+calebnartey@users.noreply.github.com>
2026-05-19 11:09:20 +0100

    certificate task list scaffold

--- test/controllers/CertificateSaoEmailControllerSpec.scala
+++ test/controllers/CertificateSaoEmailControllerSpec.scala
@@ -24,6 +24,7 @@ import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.when
 import org.scalatestplus.mockito.MockitoSugar
 import pages.CertificateSaoEmailPage
+import play.api.data.Form
 import play.api.inject.bind
 import play.api.mvc.Call
 import play.api.test.FakeRequest
@@ -32,7 +33,6 @@ import repositories.SessionRepository
 import views.html.CertificateSaoEmailView
 
 import scala.concurrent.Future
-import play.api.data.Form
 
 class CertificateSaoEmailControllerSpec extends SpecBase with MockitoSugar {
 
