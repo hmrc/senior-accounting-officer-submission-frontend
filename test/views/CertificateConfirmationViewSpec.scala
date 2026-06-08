dcd80a6da7ed0a53d87688e68b8f9911f748dd96
Su-Yang Yu <suyangyu@users.noreply.github.com>
2026-06-08 11:51:21 +0100

    SAOD-800: added missing submission button ids (#139)
    * SAOD-800: added missing submission button ids
    
    * fixed lint
    
    ---------
    
    Co-authored-by: Su-Yang Yu <20665693+suyangyu@users.noreply.github.com>

--- test/views/CertificateConfirmationViewSpec.scala
+++ test/views/CertificateConfirmationViewSpec.scala
@@ -17,6 +17,7 @@
 package views
 
 import base.ViewSpecBase
+import controllers.routes
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import views.CertificateConfirmationViewSpec.*
@@ -38,6 +39,11 @@ class CertificateConfirmationViewSpec extends ViewSpecBase[CertificateConfirmati
     )
 
     doc.createTestsWithOrWithoutError(hasError = false)
+
+    doc.createTestsWithSubmissionButton(
+      action = routes.CertificateConfirmationController.onSubmit(),
+      buttonText = "Continue"
+    )
   }
 }
 
