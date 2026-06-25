245f6e9c9115b1adfee0d24c5ad782de740c30cb
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-06-25 16:58:04 +0100

    comprehend futures, wip mapping

--- app/services/NotificationSubmitService.scala
+++ app/services/NotificationSubmitService.scala
@@ -17,12 +17,100 @@
 package services
 
 import models.UserAnswers
+import repositories.SessionRepository
+import javax.inject.Inject
+import play.api.libs.json.Json
+import uk.gov.hmrc.http.client.HttpClientV2
+import java.net.URL
+import uk.gov.hmrc.http.HttpReads.Implicits.*
+import uk.gov.hmrc.http.HeaderCarrier
+import play.api.libs.json.OFormat
+import play.api.libs.ws.WSBodyWritables.writeableOf_JsValue
+import scala.concurrent.ExecutionContext
+import uk.gov.hmrc.http.HttpResponse
+import scala.concurrent.Future
+import pages.notification.NotificationAdditionalInformationPage
+import play.api.libs.json.Reads
 
-class NotificationSubmitService {
-  def submit(userAnswers: UserAnswers): Either[String, String] = {
-    // TODO: call the protected service
-    // TODO: clear mongo on success
-    // TODO: redirect
-    ???
+class NotificationSubmitService @Inject() (httpClient: HttpClientV2, sessionRepository: SessionRepository)(using
+    ec: ExecutionContext
+) {
+  def submit(userAnswers: UserAnswers)(using hc: HeaderCarrier): Future[Either[NotificationSubmissionError, String]] = {
+    // TODO: get the url properly
+    httpClient
+      .post(URL("http://localhost:10060/notification"))
+      .withBody(Json.toJson(userAnswers.toNotification))
+      .execute[HttpResponse]
+      .flatMap { response =>
+        response.status match {
+          case 201 => {
+            sessionRepository
+              .set(userAnswers.copy(data = Json.obj()))
+              .map {
+                case true =>
+                  Right(Json.toJson(response.body).as[NotificationResponse].notificationReference)
+                case _ => Left(NotificationSubmissionError.MongoError)
+              }
+          }
+          case _ => Future.successful(Left(NotificationSubmissionError.HttpError))
+        }
+      }
   }
+}
+
+enum NotificationSubmissionError(val message: String) {
+  case MongoError extends NotificationSubmissionError("Problem with mongo")
+  case HttpError  extends NotificationSubmissionError("Problem with http client")
+}
+
+final case class NotificationRequest(
+    subscriptionId: String,
+    companies: List[Company],
+    saos: List[Sao],
+    remarks: Option[String]
+)
+
+object NotificationRequest {
+  given format: OFormat[NotificationRequest] = Json.format
+}
+
+final case class Company(
+    crn: Option[String] = None,
+    utr: String,
+    name: String,
+    accPeriodEnd: String,
+    status: String,
+    `type`: String
+)
+
+object Company {
+  given OFormat[Company] = Json.format[Company]
+}
+
+final case class Sao(
+    name: String,
+    fromDate: Option[String],
+    email: Option[String],
+    toDate: Option[String]
+)
+
+object Sao {
+  given OFormat[Sao] = Json.format[Sao]
+}
+
+extension (userAnswers: UserAnswers) {
+  def toNotification: NotificationRequest = {
+    NotificationRequest(
+      subscriptionId = "TODO where do i come from?", // TODO: figure out where subscription id comes from
+      remarks = userAnswers.getNullable(NotificationAdditionalInformationPage),
+      saos = List(),     // TODO: map saos
+      companies = List() // TODO: map companies
+    )
+  }
+}
+
+final case class NotificationResponse(notificationReference: String)
+
+object NotificationResponse {
+  given format: OFormat[NotificationResponse] = Json.format
 }
