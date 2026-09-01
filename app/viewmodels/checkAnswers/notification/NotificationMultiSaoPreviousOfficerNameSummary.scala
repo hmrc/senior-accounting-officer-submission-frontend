c5228462de02cc2f3225ca93011ea489c9153d12
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-09-01 13:35:55 +0100

    multi sao rows set up

--- app/viewmodels/checkAnswers/notification/NotificationMultiSaoPreviousOfficerNameSummary.scala
+++ app/viewmodels/checkAnswers/notification/NotificationMultiSaoPreviousOfficerNameSummary.scala
@@ -27,6 +27,7 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.converters.*
 import viewmodels.govuk.summarylist.*
 import play.twirl.api.HtmlFormat
+
 object NotificationMultiSaoPreviousOfficerNameSummary {
 
   def row(answers: UserAnswers, saoIndex: Int)(using messages: Messages): Option[SummaryListRow] =
