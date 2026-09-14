c080f188c57275136369359b61aff3f09574ec86
calebnartey <72252764+calebnartey@users.noreply.github.com>
2026-09-14 11:19:54 +0100

    more tweaks

--- app/forms/certificate/CertificateDeclarationStandInFormProvider.scala
+++ app/forms/certificate/CertificateDeclarationStandInFormProvider.scala
@@ -21,13 +21,9 @@ import models.certificate.CertificateDeclarationStandIn
 import play.api.data.Form
 import play.api.data.Forms.*
 
-import scala.util.matching.Regex
-
 import javax.inject.Inject
 
 class CertificateDeclarationStandInFormProvider @Inject() extends Mappings {
-
-  val illegalCharsRegex: Regex = """[<>"]""".r
 
   def apply(): Form[CertificateDeclarationStandIn] = Form(
     mapping(
