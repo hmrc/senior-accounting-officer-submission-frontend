a0eeb2c69f10babd6ab72af4a5075f28389bc8b7
jacob-leeming-netcompany <235530878+jacob-leeming-netcompany@users.noreply.github.com>
2026-09-16 10:22:18 +0100

    add missing html escape

--- app/viewmodels/checkAnswers/notification/NotificationMultiSaoPreviousOfficerNameSummary.scala
+++ app/viewmodels/checkAnswers/notification/NotificationMultiSaoPreviousOfficerNameSummary.scala
@@ -26,7 +26,7 @@ import uk.gov.hmrc.govukfrontend.views.viewmodels.content.HtmlContent
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
 import viewmodels.converters.*
 import viewmodels.govuk.summarylist.*
-
+import play.twirl.api.HtmlFormat
 object NotificationMultiSaoPreviousOfficerNameSummary {
 
   def row(answers: UserAnswers, saoIndex: Int)(using messages: Messages): Option[SummaryListRow] =
@@ -45,8 +45,8 @@ object NotificationMultiSaoPreviousOfficerNameSummary {
               "notificationMultiSaoPreviousOfficerName.checkYourAnswersLabel",
               priorSaoName
             ).toKey,
-            value =
-              ValueViewModel(HtmlContent(s"""<span data-test-id="previous-sao-name-${saoIndex + 1}">$answer</span>""")),
+            value = ValueViewModel(HtmlContent(s"""<span data-test-id="previous-sao-name-${saoIndex + 1}">${HtmlFormat
+                .escape(answer)}</span>""")),
             actions = Seq(
               ActionItemViewModel(
                 messages("site.change").toText,
